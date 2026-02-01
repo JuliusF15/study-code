4ce911a2dbef173e84885e41ac9373a3930769c6
Julius <faustmail@duck.com>
2026-02-01 13:32:52 +0100

    Used different counter for Rows

--- courses/coding-in-C/Lab_2/solution/mysolution/ChessBoard.c
+++ courses/coding-in-C/Lab_2/solution/mysolution/ChessBoard.c
@@ -4,10 +4,10 @@ int numRows = 8;
 int numCols = 8;
 
 int main(){
-    for(int i=0; i<numRows; i++){
+    for(int i=numRows; i>0; i--){
         for(int e=0; e<numCols; e++){
-            printf("%c", 65+e); //Print Letter
-            printf("%d %c", numRows-i, " ");
+            printf("%c", 65+e); //Print Letter using ASCII of A plus the current Collumn
+            printf("%d %c", i, " "); //Print Number using current Row
 
         }
         printf("\n");
