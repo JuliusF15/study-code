1567b961f0316d9ebea3086524a06261087ecf65
Julius <faustmail@duck.com>
2026-02-03 12:34:41 +0100

    Added and begun with ChessBoardBig.c

---
+++ courses/coding-in-C/Lab_2/solution/mysolution/ChessBoardBig.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int numRows = 8;
+int numCols = 8;
+
+void printLine(){
+    printf("\n");
+    for(int i=0; i<numCols;i++){
+        printf("%c" "+---");
+    }
+    //printf("%c", "+");
+}
+
+int main(){
+    for(int e=0; e<numCols; e++){
+        printf("%c %c %c", " ", 65+e, " "); //Print Letter
+    }
+    printLine();
+
+}
+
+/*
+#include <stdio.h>
+
+int numRows = 8;
+int numCols = 8;
+
+void printLine(){
+    //printf("\n");
+    for(int i=0; i<numCols;i++){
+        printf("%c" "+---");
+    }
+    printf("%c" "+");    
+    //printf("\n");
+}
+int main(){
+    for(int e=0; e<numCols; e++){
+        printf("%c %c %c", " ", 65+e, " "); //Print Letter
+    }
+    printf("\n");
+    printf("\n");
+    printLine();
+
+}*/
