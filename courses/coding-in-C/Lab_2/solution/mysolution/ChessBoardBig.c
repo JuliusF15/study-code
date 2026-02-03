14e7d15a8300193ad2a4c52fc403a4632cd91c9a
Julius <faustmail@duck.com>
2026-02-03 22:09:54 +0100

    almost finnished

--- courses/coding-in-C/Lab_2/solution/mysolution/ChessBoardBig.c
+++ courses/coding-in-C/Lab_2/solution/mysolution/ChessBoardBig.c
@@ -3,21 +3,44 @@
 int numRows = 8;
 int numCols = 8;
 
+void printColLetters(){
+    printf("%c", 32);
+    for(int e=0; e<numCols; e++){
+        printf("%c %c %c", 32, 65+e, 31); //Print Letter
+    }
+}
 void printLine(){
     printf("\n");
+    printf("%c", 32);
     for(int i=0; i<numCols;i++){
-        printf("%c", "+");
+        printf("%c", 43);
         printf("%c" "---");
-        printf("%c", "+");
+    }
+    printf("%c", 43);
+
+}
+void printRows(){
+    printf("\n");
+    for(int i=numRows; i>0; i--){
+        printf("%d", i);
+        printf("%c", 124);
+    
+        for(int e=0; e<numCols;e++){
+            if((i+e)%2==0){
+                printf("%c%c%c", 35, 35, 35);
+            }else{
+                printf("%c %c  %c", "", "", "");
+            }
+            printf("%c", 124);
+        }
+        printf("\n");
     }
 }
 
 int main(){
-    for(int e=0; e<numCols; e++){
-        printf("%c %c %c", " ", 65+e, " "); //Print Letter
-    }
+    printColLetters();
     printLine();
-
+    printRows();
 }
 
 /*
