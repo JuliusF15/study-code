35985421730e5bcd237c88b5b5fbf7d52254a73d
Julius <faustmail@duck.com>
2026-02-04 08:42:52 +0100

    Added Big Constants

--- courses/coding-in-C/Lab_2/solution/mysolution/ChessBoardBig.c
+++ courses/coding-in-C/Lab_2/solution/mysolution/ChessBoardBig.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int numRows = 8;
-int numCols = 8;
+int NUMROWS = 8;
+int NUMCOLS = 8;
 
 void printColLetters(){
     printf("%c%c%c", 32, 32, 32);
-    for(int e=0; e<numCols; e++){
+    for(int e=0; e<NUMCOLS; e++){
         printf("%c%c%c%c", 32, 65+e, 32, 32); //Print Letter
     }
 }
 void printLine(){
     printf("\n");
     printf("%c ", 32);
-    for(int i=0; i<numCols;i++){
+    for(int i=0; i<NUMCOLS;i++){
         printf("%c", 43);
         printf("%c" "---");
     }
@@ -21,11 +21,11 @@ void printLine(){
 }
 void printRows(){
     printf("\n");
-    for(int i=numRows; i>0; i--){
+    for(int i=NUMROWS; i>0; i--){
         printf("%d ", i);
         printf("%c", 124);
     
-        for(int e=0; e<numCols;e++){
+        for(int e=0; e<NUMCOLS;e++){
             if((i+e)%2==0){
                 printf("%c%c%c", 35, 35, 35);
             }else{
