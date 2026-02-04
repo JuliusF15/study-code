92b071e668f987fac9018242022c935e1f15e354
Julius <faustmail@duck.com>
2026-02-04 10:27:40 +0100

    Working Calculator with switch case

--- courses/coding-in-C/Lab_3/solution/mySolution/Calculator.c
+++ courses/coding-in-C/Lab_3/solution/mySolution/Calculator.c
@@ -6,34 +6,42 @@ int main(){
     float Input2;
     float Output;
     char Operation;
+    while(1){
 
-    printf("Number1: ");
-    scanf("%f", &Input1);
-    printf("\nNumber2: ");
-    scanf("%f", &Input2);
+        printf("\nPress Strg+C to Exit...");
+        printf("\nNumber1: ");
+        scanf("%f", &Input1);
+        printf("\nNumber2: ");
+        scanf("%f", &Input2);
 
-    printf("\nChoose Operation(+, -, *, /): ");
-    scanf(" %c", &Operation);
+        printf("\nChoose Operation(+, -, *, /): ");
+        scanf(" %c", &Operation);
 
-    switch (Operation)
-    {
-    case '+': // +
-        Output = Input1 + Input2;
-        printf("%f", Output);
-        break;
-    case '-': // -
-        Output = Input1 - Input2;
-        printf("%f", Output);        break;
-    case '*': //*
-        Output = Input1 * Input2;
-        printf("%f", Output);         break;
-    case '/': // /
-        Output = Input1 / Input2;
-        printf("%f", Output);
-        break;
-    default:
-        printf("\nNot a Valid Operation!");
-        break;
+        switch (Operation)
+        {
+        case '+': // +
+            Output = Input1 + Input2;
+            printf("%f", Output);
+            break;
+        case '-': // -
+            Output = Input1 - Input2;
+            printf("%f", Output);        break;
+        case '*': //*
+            Output = Input1 * Input2;
+            printf("%f", Output);         break;
+        case '/': // /
+            if(Input2 == 0){
+                printf("Can't divide with 0");
+                break;
+            }else{
+                Output = Input1 / Input2;
+                printf("%f", Output);
+                break;
+            }
+        default:
+            printf("\nNot a Valid Operation!");
+            break;
+        }
     }
 
 }
