fb798bbdbf6769626a29beff9e850149df1a62ff
Julius <faustmail@duck.com>
2026-02-12 18:03:27 +0100

    startet palindrome.c

--- courses/coding-in-C/Lab_4/solution/mysolution/palindrome.c
+++ courses/coding-in-C/Lab_4/solution/mysolution/palindrome.c
@@ -1,5 +1,11 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(){
-    
+    char text[10];
+    char *ptext = text;
+    printf("\nGive me a Word. Max. 100 Characters: ");
+    fgets(text, sizeof(text), stdin);
+    text[strcspn(text, "\n")] = '\0';
+    printf("%s", text);
 }
